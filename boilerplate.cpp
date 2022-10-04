e91b7f2f1f40bbc0d1b463edef87f92073909a24
Kushagra1974 <kamle.kush2002@gmail.com>
2022-10-04 23:24:34 +0530

    boilerplate template

---
+++ boilerplate.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define ll long long;
+#define vvi vector<vector<int>>;
+
+using namespace std;
+
+void solve();
+
+int main()
+{
+    int n = 1;
+    cin >> n;
+    while (n--)
+    {
+        solve();
+    }
+}
+
+void solve()
+{
+}
